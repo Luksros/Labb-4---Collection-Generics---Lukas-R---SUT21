04a97935ed450ff6fc08a324593c7bf973a66d73
Luksros <lukasrose@live.se>
2021-11-22 00:00:36 +0100

    Add project files.

---
+++ Labb 4 - Collection Generics - Lukas R - SUT21/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_4___Collection_Generics___Lukas_R___SUT21
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public float Salary { get; set; }
+        public Employee(int id, string first, string last, char gender, float salary)
+        {
+            this.Id = id;
+            this.FirstName = first;
+            this.LastName = last;
+            this.Gender = gender;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return ("ID: " + Id + " - Namn: " + FirstName + " " + LastName + " - Kön: " + Gender + " - Lön: " + Salary);
+        }
+    }
+}
---
+++ Labb 4 - Collection Generics - Lukas R - SUT21/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb_4___Collection_Generics___Lukas_R___SUT21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Employee> empStack = new Stack<Employee>();
+            Employee lukas = new Employee(101, "Lukas", "Rose", 'M', 250000f);
+            Employee erikN = new Employee(207, "Erik", "Norell", 'M', 260500f);
+            Employee viktor = new Employee(183, "Viktor", "Gunnarsson HK", 'M', 272300f);
+            Employee erikR = new Employee(214, "Erik", "Risholm", 'M', 260000f);
+            Employee helga = new Employee(142, "Helga", "Hildenbrau", 'F', 302000f);
+
+            empStack.Push(lukas);
+            empStack.Push(erikN);
+            empStack.Push(viktor);
+            empStack.Push(erikR);
+            empStack.Push(helga);
+
+            WriteGreen("STACK.COUNT:");
+            foreach (Employee e in empStack)
+            {
+                Console.WriteLine(e);
+                WriteYellow("Föremål kvar i stack: " + empStack.Count);
+            }
+            Continue();
+
+            WriteGreen("STACK.POP:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(empStack.Pop());
+                WriteYellow("Föremål kvar i stack: " + empStack.Count);
+            }
+            Continue();
+
+            empStack.Push(lukas);
+            empStack.Push(erikN);
+            empStack.Push(viktor);
+            empStack.Push(erikR);
+            empStack.Push(helga);
+
+            WriteGreen("STACK.PEEK");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(empStack.Peek()); 
+                WriteYellow("Föremål kvar i stack: " + empStack.Count);
+            }
+            Continue();
+
+            WriteGreen("Is item 3 in stack?");
+            Console.WriteLine((empStack.Contains(viktor)) ? "\nItem 3  is in stack" : "\nItem 3 is not in stack");
+            Console.Clear();
+
+            List<Employee> empList = new List<Employee>() { lukas, erikN, erikR, viktor, helga};
+
+            WriteGreen("LIST: ");
+            Console.WriteLine((empList.Contains(viktor) ? "Viktor exists in the list\n" : "Viktor does not exist in the list\n"));
+            WriteYellow("First male employee in list:");
+            Console.WriteLine(empList.Find(emp => emp.Gender == 'M'));
+            List<Employee> maleEmpList = new List<Employee>(empList.FindAll(emp => emp.Gender == 'M'));
+
+            WriteYellow("\nAll male employees: ");
+            foreach  (Employee employee in maleEmpList)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.ReadLine();
+
+        }
+        public static void WriteYellow(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(input);
+            Console.ResetColor();
+        }
+
+        public static void WriteGreen(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(input);
+            Console.ResetColor();
+        }
+
+        public static void Continue()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Tryck Enter för att fortsätta.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
